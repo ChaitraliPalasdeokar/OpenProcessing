a5ba6f3f5756fdf88529d5b42a462d164eb0e9f4
ChaitraliPalasdeokar <chaitrali.palasdeokar@gmail.com>
2021-05-31 08:24:38 +0530

    Cleaned code

--- src/main/java/TestProcessing_OOP.java
+++ src/main/java/TestProcessing_OOP.java
@@ -1,67 +1,66 @@
 import processing.core.PApplet;
+
+import javax.jws.soap.SOAPBinding;
+
 public class TestProcessing_OOP extends PApplet{
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
     public static final int TOTAL_NO_OF_BALLS = 4;
 
     Ball balls[]=new Ball[TOTAL_NO_OF_BALLS];
-
     public static void main(String[] args) {
         PApplet.main("TestProcessing_OOP",args);
-//        String[] processingArgs = {"TestProcessing_OOP"};
-//        TestProcessing_OOP sketch = new TestProcessing_OOP();
-//        PApplet.runSketch(processingArgs,sketch);
-
-
     }
 
     @Override
     public void settings() {
         super.settings();
         size(WIDTH,HEIGHT);
-        for(int i=0;i<TOTAL_NO_OF_BALLS;i++){
-            balls[i]=new Ball(this,i+1,i+1);
-        }
+
     }
 
     @Override
     public void setup() {
         super.setup();
+        for(int itr=0;itr<TOTAL_NO_OF_BALLS;itr++) {
+            balls[itr]=new Ball(this,itr+1,itr+1);
+        }
+
     }
 
     @Override
     public void draw() {
-        super.draw();
+        getBackground();
         for(int itr=0;itr<TOTAL_NO_OF_BALLS;itr++) {
-            balls[itr].drawCircle();
+            balls[itr].drawMovingCircle();
         }
-        //System.out.println(balls[0].height+" "+balls[0].speed);
-        //balls[0].drawCircle();
-        //balls[0].moveCircle();
+    }
+
+    private void getBackground() {
+        background(0);
     }
 
 
 }
 
 class Ball {
-    PApplet sketch;
-    int height,speed,movecircle=0;
-    public static final int DIAMETER = 10;
+   private PApplet sketch;
+    private int height,speed,startposition=0;
+    public static final int DIAMETER = 20;
 
     Ball(PApplet sketch,int height,int speed){
+        this.startposition=startposition;
         this.sketch=sketch;
         this.height=height;
         this.speed=speed;
     }
 
-    void drawCircle(){
-        //System.out.println(TestProcessing_OOP.HEIGHT);
-        System.out.println(movecircle);
-        sketch.ellipse(movecircle,height*(TestProcessing_OOP.HEIGHT/5),DIAMETER,DIAMETER);
-        movecircle+=speed;
+    void drawMovingCircle(){
+
+        sketch.ellipse(this.startposition,height*(TestProcessing_OOP.HEIGHT/5),DIAMETER,DIAMETER);
+        this.startposition=this.startposition+this.speed;
+
     }
-    void moveCircle(){
-        movecircle+=speed;
-    }
+
 }
 
